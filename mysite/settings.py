e65f7a6fe7bdfd7e6db1357cb0289cc17c775222
demetrius <d.natxa@gmail.com>
2021-08-14 23:21:06 +0300

    start auth and friendship

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,10 +127,7 @@ LANGUAGE_PATHS = [
 ]
 
 
-
-
-
-
+## Photos
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
@@ -143,7 +140,6 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+# Auth
 
-
-
-
+AUTH_USER_MODEL = 'users.User'
