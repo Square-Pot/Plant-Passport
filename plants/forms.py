0754f2c82f8277b8c519af923460ac201d684bf6
spalk <d.natxa@gmail.com>
2021-08-29 21:57:12 +0000

    create plant code roveiw

--- plants/forms.py
+++ plants/forms.py
@@ -4,34 +4,41 @@ from .models import Attribute, Plant, RichPlant, Photo
 from django.forms import ModelForm
 
 
+
 class PlantForm(forms.Form):
     """
     Generate form fields for all plant attributes
     """
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        for attribute in Attribute.objects.all():
-            widget = None
+        for attribute in Attribute.objects.all().order_by('weight'):
+            # String
+            if attribute.value_type == Attribute.AttributeTypeChoices.STRING:
+                self.fields[attribute.key] = forms.CharField(label=attribute.name, max_length=attribute.max_text_length, required=False)
+            # Textarea
             if attribute.value_type == Attribute.AttributeTypeChoices.TEXTAREA:
-                widget = forms.Textarea
+                self.fields[attribute.key] = forms.CharField(label=attribute.name, max_length=attribute.max_text_length, required=False, widget=forms.Textarea)
+            # Date
             if attribute.value_type == Attribute.AttributeTypeChoices.DATE:
-                widget = forms.TextInput(attrs={'type': 'date'})
-            self.fields[attribute.key] = forms.CharField(label=attribute.name, max_length=100, required=False, widget=widget)    
-
+                self.fields[attribute.key] = forms.DateField(label=attribute.name, required=False, widget=forms.widgets.DateInput(attrs={'type': 'date'}))
+            # Number
+            if attribute.value_type == Attribute.AttributeTypeChoices.NUMBER:
+                self.fields[attribute.key] = forms.DecimalField(label=attribute.name, max_digits=5, required=False)
+            
 
 class AttributeForm(forms.Form):
     """
     Form for editing plant attribute
     """
+    
     def __init__(self, label=None, key=None, value=None, max_length=None, type=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
         widget = None
         if type == Attribute.AttributeTypeChoices.TEXTAREA:
             widget = forms.Textarea
-        if type == Attribute.AttributeTypeChoices.DATE:
-            widget = forms.TextInput(attrs={'type': 'date'})
         self.fields[key] = forms.CharField(label=label, initial=value, max_length=max_length, required=False, widget=widget)
-
+        
+        
 
 
 class PhotoForm(forms.ModelForm):
