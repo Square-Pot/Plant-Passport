5ee7ceca618f781b4470402af48ee0a960c0e69b
spalk <d.natxa@gmail.com>
2021-09-29 13:48:06 +0000

    add: pdf app and view

---
+++ pdf/services.py
@@ -0,0 +1,8 @@
+
+
+
+
+
+def generate_labels_pdf(rich_plants:list):
+
+    return "path_to_file"
---
+++ pdf/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+from django.http import HttpResponse, Http404, HttpResponseForbidden
+from django.contrib.auth.decorators import login_required
+from plants.models import Plant
+from plants.entities import RichPlant
+from plants.services import check_is_user_owner_of_plant
+from .services import generate_labels_pdf
+
+@login_required
+def get_labels_pdf(request, plant_ids):
+
+    # processing user data
+    if request.method == 'POST':
+
+        current_user = request.user
+
+        plants = Plant.objects.filter(id__in=plant_ids)
+        rich_plants = []
+        for plant in plants: 
+            rich_plants.append(RichPlant(plant))
+
+        # check access (is owner) for all plants
+        for rich_plant in rich_plants: 
+            user_is_owner = check_is_user_owner_of_plant(current_user, rich_plant)
+            if not user_is_owner:
+                return HttpResponseForbidden()
+
+        
+        # generate pdf and take path to it
+        path_to_pdf = generate_labels_pdf(rich_plants)
+        if path_to_pdf:
+            filename = 'user_labels.pdf'
+            pdf_file = open(path_to_pdf, 'rb')
+            response = HttpResponse(content=pdf_file)
+            response['Content-Type'] = 'application/pdf'
+            response['Content-Disposition'] = 'attachment; filename="%s.pdf"' % filename
+            return response
+
+    
+    else:
+        return Http404("No plants was received")
