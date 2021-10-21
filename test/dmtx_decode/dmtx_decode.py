09a92097b4605d57526ef3def5741b838e3a3c36
spalk <d.natxa@gmail.com>
2021-10-21 14:17:11 +0000

    tests

--- test/dmtx_decode/dmtx_decode.py
+++ test/dmtx_decode/dmtx_decode.py
@@ -1,8 +1,13 @@
+#!/usr/bin/env python
 import numpy as np
 import cv2
 from pylibdmtx import pylibdmtx
 
-image = cv2.imread('example.jpg', cv2.IMREAD_UNCHANGED);
+# has data matrix
+#image = cv2.imread('example.jpg', cv2.IMREAD_UNCHANGED)
+
+# has not data matrix
+image = cv2.imread('bad.png', cv2.IMREAD_UNCHANGED)
 
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
@@ -10,4 +15,10 @@ ret,thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 
 msg = pylibdmtx.decode(thresh)
 
+#msg = pylibdmtx.decode(image)
+
 print(msg)
+#print(int(msg[0].data))
+for i in msg:
+    puid = i.data.decode("utf-8") 
+    print(puid)
