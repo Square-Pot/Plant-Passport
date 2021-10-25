88529a6cab0f4379873b36c74b0e59db54752576
spalk <d.natxa@gmail.com>
2021-10-25 19:02:21 +0000

    bugfix

--- plants/services.py
+++ plants/services.py
@@ -255,7 +255,7 @@ def check_is_user_owner_of_plant(user, target_rich_plant):
 
 # Logs
 
-def create_log(action_type: Log.ActionChoices, user: User, plant: Plant, data: dict, action_time: None):
+def create_log(action_type: Log.ActionChoices, user: User, plant: Plant, data: dict, action_time=None):
     """Create new log"""
 
     # selialize date fields
